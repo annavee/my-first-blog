5186335e37be0e6399759845ed966a35997bf992
Anna Vee <annavee8@gmail.com>
2019-10-19 14:51:44 +1100

    My Django Girls app, first commit

---
+++ Tutorial Coding/python_intro 1.py
@@ -0,0 +1,28 @@
+print('Hello, Django girls!')
+if 3 > 2:
+    print('It works!')
+if 5 > 2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
+name = 'Caleigh Mary'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Caleigh Mary':
+    print('Hey Caleigh Mary!')
+else:
+    print('Hey anonymous!')
+volume = 57
+if volume < 20:
+    print("It's kinda quiet.")
+elif 20 <= volume < 40:
+    print("It's nice for background music")
+elif 40 <= volume < 60:
+    print("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+    print("Nice for parties")
+elif 80 <= volume < 100:
+    print("A bit loud!")
+else:
+    print("My ears are hurting! :(")
+
